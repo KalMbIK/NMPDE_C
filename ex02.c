07bcbc926a0e9499450b451aced1432deb08a4e8
KalMbIK <dekalmykov7@gmail.com>
2017-11-11 20:38:14 +0100

    corrected

--- ex02.c
+++ ex02.c
@@ -1,3 +1,6 @@
+//
+// Created by pusheen on 30.10.17.
+//
 //#define TYPE int
 #include <math.h>
 #include <stdio.h>
@@ -5,7 +8,10 @@
 #include <string.h>
 #include "myNumPy.h"
 
-int N = 81;
+
+//IMPORTANT: I used notation from the Wikipedia to declare the TDM
+
+int N = 20;
 double x0=0, x1 = 1;
 double T = 0.2;
 double a_diff = 1;
@@ -22,7 +28,8 @@ double exactSolution(double x){
     return u0(x)*f2(T);
 }
 
-void TDA_solver(const double *a, const double *b, const double *c, double *cPrime, const double *d, double *dPrime, double *result, int size) {
+void TDMA_solver(const double *a, const double *b, const double *c, double *cPrime, const double *d, double *dPrime,
+                 double *result, int size) {
     cPrime[0] = c[0]/b[0];
     dPrime[0] = d[0]/b[0];
     for (int i = 1; i < size; i++){
@@ -80,7 +87,7 @@ void solver(double lambda, double t, double* u0, double * res, int size){
     double tt = 0;
     while (tt < T){
         generateRightHandSide(aRight, bRight, cRight, u, d, size);
-        TDA_solver(a, b, c, cPrime, d, dPrime, v, size);
+        TDMA_solver(a, b, c, cPrime, d, dPrime, v, size);
         tt += t;
         double* temp = u;
         u = v;
@@ -111,17 +118,17 @@ double getError(double* a, double* b, int size){
 }
 
 int main(void){
-    double h = (x1-x0)/(N-1);
+    double h = (x1-x0)/(N);
     double t = h;
     double la = a_diff*t/h/h;
-    double * gr = linspace(x0,x1,N);
-    double * u = vectorize(&u0,gr,N);
-    double * v = newArray(N);
-    double * exact = vectorize(&exactSolution,gr,N);
+    double * gr = linspace(x0,x1,N+1);
+    double * u = vectorize(&u0,gr,N+1);
+    double * v = newArray(N+1);
+    double * exact = vectorize(&exactSolution,gr,N+1);
 
-    solver(la,t,&u[1],&v[1],N-2);
+    solver(la,t,&u[1],&v[1],N-1);
 
-    printf("Error = %g", getError(v,exact,N));
+    printf("Error = %g", getError(v,exact,N+1));
 
     free(gr);
     free(u);
